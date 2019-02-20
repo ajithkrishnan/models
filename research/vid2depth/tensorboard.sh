3d21e8ba6f33cc6b5fb60aff3786c413218e2524
Ajith Krishnan <ajithkrishnanbm@gmail.com>
2019-02-20 10:47:13 +0100

    Added shell scripts for training and tensorboard

--- research/vid2depth/tensorboard.sh
+++ research/vid2depth/tensorboard.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-tensorboard --logdir=/home/trn_ak/git_clones/models/research/vid2depth/checkpoints --port=6007
+tensorboard --logdir=current_run:/home/trn_ak/git_clones/models/research/vid2depth/checkpoints_default --port=6008
+#tensorboard --logdir=run1:/home/trn_ak/git_clones/models/research/vid2depth/checkpoints,run2:/home/trn_ak/git_clones/models/research/vid2depth/checkpoints_default --port=6008
--- research/vid2depth/train.sh
+++ research/vid2depth/train.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-python2 $PATH_VID2DEPTH/train.py \
+python $PATH_VID2DEPTH/train.py \
       --data_dir $PATH_VID2DEPTH/data/kitti_raw_eigen \
       --seq_length 3 \
       --reconstr_weight 0.85 \
       --smooth_weight 0.05 \
       --ssim_weight 0.15 \
-      --icp_weight 0 \
-      --checkpoint_dir $PATH_VID2DEPTH/checkpoints 
-#      --pretrained_ckpt $PATH_VID2DEPTH/checkpoints/model-29465 \
+      --icp_weight 0.0 \
+      --train_steps 119496 \
+      --batch_size 4 \
+      --checkpoint_dir $PATH_VID2DEPTH/checkpoints_icp
 
+#      --pretrained_ckpt $PATH_VID2DEPTH/checkpoints_default/last_model \
 
 
