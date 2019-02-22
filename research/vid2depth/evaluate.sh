32b402a9784959f5c9ebf44f5108661f8967a2fe
Ajith Krishnan <ajithkrishnanbm@gmail.com>
2019-02-22 17:34:08 +0100

    updated evaluate.sh

--- research/vid2depth/evaluate.sh
+++ research/vid2depth/evaluate.sh
@@ -6,5 +6,6 @@
 python $PATH_VID2DEPTH/evaluate.py \
     --split kitti \
     --prediction_path $PATH_VID2DEPTH/inference_validation_egomotion/ \
-    --gt_path $PATH_KITTI/2011_09_26/2011_09_26_drive_0009_sync/oxts/data
+    --gt_path $PATH_KITTI_ODOM/2011_09_26/2011_09_26_drive_0009_sync/oxts/data
 
+#    --gt_path $PATH_KITTI/2011_09_26/2011_09_26_drive_0009_sync/oxts/data
