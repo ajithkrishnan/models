9840affa4e7a30feed3b3716dac7dd88ee10de94
ajithkrishnan <ajithkrishnanbm@gmail.com>
2019-03-02 18:18:56 +0100

    TODO: update load_sequence()

--- research/vid2depth/kitti_eval/gen_gtruth.py
+++ research/vid2depth/kitti_eval/gen_gtruth.py
@@ -13,24 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 
-"""Generates depth estimates for an entire KITTI video."""
-
-# Example usage:
-#
-# python inference.py \
-#   --logtostderr \
-#   --kitti_dir ~/vid2depth/kitti-raw-uncompressed \
-#   --kitti_video 2011_09_26/2011_09_26_drive_0009_sync \
-#   --output_dir ~/vid2depth/inference \
-#   --model_ckpt ~/vid2depth/trained-model/model-119496
-#
-# python inference.py \
-#   --logtostderr \
-#   --kitti_dir ~/vid2depth/kitti-raw-uncompressed \
-#   --kitti_video test_files_eigen \
-#   --output_dir ~/vid2depth/inference \
-#   --model_ckpt ~/vid2depth/trained-model/model-119496
-#
+"""Generates ground truth data for evaluation."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -47,6 +30,7 @@ import scipy.misc
 import tensorflow as tf
 import util
 from kitti_eval.pose_evaluation_utils import dump_pose_seq_TUM
+import csv
 
 gfile = tf.gfile
 
@@ -57,134 +41,107 @@ DEFAULT_MODE = 'depth'
 
 flags.DEFINE_string('output_dir', DEFAULT_OUTPUT_DIR,
                     'Directory to store estimated depth maps.')
-flags.DEFINE_string('kitti_dir', DEFAULT_KITTI_DIR, 'KITTI dataset directory.')
-flags.DEFINE_string('kitti_video', None, 'KITTI video directory name.')
+flags.DEFINE_string('gt_dir', DEFAULT_KITTI_DIR, 'KITTI dataset directory.')
+flags.DEFINE_string('kitti_sequence', None, 'KITTI video directory name.')
 flags.DEFINE_integer('seq_length', 3, 'Sequence length for each example.')
+flags.DEFINE_string('mode', DEFAULT_MODE, 'Specify the network to run inference on i.e depth or pose' )
 
 FLAGS = flags.FLAGS
 
-flags.mark_flag_as_required('kitti_video')
+flags.mark_flag_as_required('gt_dir')
+flags.mark_flag_as_required('kitti_sequence')
+flags.mark_flag_as_required('seq_length')
 
-def _run_inference():
-  """Runs all images through depth model and saves depth maps."""
-  output_dir = FLAGS.output_dir
-  if not gfile.Exists(output_dir):
-    gfile.MakeDirs(output_dir)
 
-  inference_model = model.Model(is_training=False,
-                                seq_length=FLAGS.seq_length,
-                                batch_size=FLAGS.batch_size,
-                                img_height=FLAGS.img_height,
-                                img_width=FLAGS.img_width)
+def _gen_data():
 
-  sv = tf.train.Supervisor(logdir='/tmp/', saver=None)
-
-  max_src_offset = (FLAGS.seq_length - 1)//2
-
-  with open(FLAGS.kitti_dir + 'sequences/%.2d/times.txt' % int(FLAGS.kitti_video), 'r') as f:
-      times = f.readlines()
-  times = np.array([float(s[:-1]) for s in times])
-
-  with sv.managed_session() as sess:
-    saver.restore(sess, FLAGS.model_ckpt)
-    if FLAGS.kitti_video == 'test_files_eigen':
-      im_files = util.read_text_lines(
-          util.get_resource_path('dataset/kitti/test_files_eigen.txt'))
-      im_files = [os.path.join(FLAGS.kitti_dir, f) for f in im_files]
+    gt_path = os.path.join(FLAGS.kitti_dir, '%.2d_full.txt' % FLAGS.kitti_sequence)
+    if not os.path.exists(gt_path) :
+        break
     else:
-      video_path = os.path.join(FLAGS.kitti_dir, 'sequences/', FLAGS.kitti_video)
-#      im_files = gfile.Glob(os.path.join(video_path, 'image_02/data', '*.png'))
-      im_files = gfile.Glob(os.path.join(video_path, 'image_2/', '*.png'))
-      im_files = [f for f in im_files if 'disp' not in f]
-      im_files = sorted(im_files)
+        with open(gt_path) as gt_file:
+            gt_reader = list(csv.reader(gt_file, delimiter=' '))
 
-    egomotion_file = None    
-  
-    with gfile.Open(os.path.join(output_dir, 'inference.txt'), 'w') as inf_egomotion_f:
-      for i in range(0, len(im_files), FLAGS.batch_size):
-        if i % 100 == 0:
-          logging.info('Generating from %s: %d/%d', ckpt_basename, i,
-                      len(im_files))
+            times = []
+            for j, _ in enumerate(gt_reader):
+                gt_reader[j] = [float(i) for i in gt_reader[j]]
+                times.append(g_row[0])
 
-        inputs_depth = np.zeros(
-            (FLAGS.batch_size, FLAGS.img_height, FLAGS.img_width, 3),
-            dtype=np.uint8)
-        inputs_egomotion = np.zeros(
-            (FLAGS.batch_size, FLAGS.img_height, FLAGS.img_width *  FLAGS.seq_length, 3),
-            dtype=np.uint8)
-        
-        if FLAGS.mode == 'depth':
-            inputs = inputs_depth
-        elif FLAGS.mode == 'egomotion':
-            inputs = inputs_egomotion
+    #        max_offset = (FLAGS.seq_length - 1)//2
+            gt_reader = np.array(gt_reader)
+            times = np.array(times)
+            test_frames = ['%.2d %.6d' % (int(FLAGS.kitti_sequence), n) for n in range(len(gt_reader))]
+            max_offset = 1
+          
+            for tgt_idx in range(0, len(gt_reader)):
 
-        for b in range(FLAGS.batch_size):
-          idx = i + b
-          if idx >= len(im_files):
-            break
-          im = scipy.misc.imread(im_files[idx])
+                if not is_valid_sample(test_frames, tgt_idx, FLAGS.seq_length):
+                  continue
+                if tgt_idx % 100 == 0:
+                  logging.info('Generating from %s: %d/%d', gt_path, tgt_idx,
+                              len(gt_reader))
 
-          if FLAGS.mode == 'depth':
-            inputs[b] = scipy.misc.imresize(im, (FLAGS.img_height, FLAGS.img_width))
-          elif FLAGS.mode == 'egomotion':
-            inputs[b] = scipy.misc.imresize(im, (FLAGS.img_height, 
-                FLAGS.img_width * FLAGS.seq_length))
-        
-        results = inference_model.inference(inputs, sess, mode=FLAGS.mode)
+                # TODO: currently assuming batch_size = 1
 
-        for b in range(FLAGS.batch_size):
-          idx = i + b
-          if idx >= len(im_files):
-            break
+                pose_seq = load_sequence(FLAGS.gt_dir, 
+                                                test_frames, 
+                                                tgt_idx, 
+                                                FLAGS.seq_length)
 
-          if FLAGS.mode == 'depth':
-              if FLAGS.kitti_video == 'test_files_eigen':
-                depth_path = os.path.join(output_dir, '%03d.png' % idx)
-              else:
-                depth_path = os.path.join(output_dir, '%04d.png' % idx)
-              depth_map = results['depth'][b]
-              depth_map = np.squeeze(depth_map)
-              colored_map = _normalize_depth_for_display(depth_map, cmap=CMAP)
-              input_float = inputs[b].astype(np.float32) / 255.0
-              vertical_stack = np.concatenate((input_float, colored_map), axis=0)
-              scipy.misc.imsave(depth_path, vertical_stack)
+                egomotion_data = pose_seq
+                # Insert target poses
+                # DEBUG: check if the target pose is at the right index
+                #        egomotion_data = np.insert(egomotion_data, 0, np.zeros((1,6)), axis=0) 
+                #        egomotion_data = np.insert(egomotion_data, 2, np.zeros((1,6)), axis=0) 
+                egomotion_data = np.insert(egomotion_data, max_offset, np.zeros((1,6)), axis=0) 
+                curr_times = times[tgt_idx - max_offset:tgt_idx + max_offset + 1]
+                egomotion_file = FLAGS.output_dir + '%.6d.txt' % (tgt_idx - max_offset)
+                #        egomotion_path = os.path.join(FLAGS.output_dir, str(egomotion_file))
+                for j, g_row in enumerate(gt_reader):
+                with open(os.path.join(FLAGS.output_dir, '%.6d.txt' % j),'w') as f:
+                        writer = csv.writer(f, delimiter=' ')
+                #                        writer.writerows(pose_seq)
+                        writer.writerow(pose_seq)
 
-          elif FLAGS.mode == 'egomotion':
-              for j in range(FLAGS.seq_length - 1):
-#                  if FLAGS.kitti_video == 'test_files_eigen':
-#                      egomotion_path = os.path.join(output_dir, '%i%d.txt' % (idx,j))
-#                  else:
-#                      egomotion_path = os.path.join(output_dir, '%04d.txt' % (idx+j))
-#
-#                  egomotion_file = gfile.Open(egomotion_path, 'w')
-#                  egomotion_data = results['egomotion'][b]
-#                  egomotion_data = np.squeeze(egomotion_data)
-#                  egomotion_file.write(' '.join(str(d) for d in egomotion_data[j]))
 
-                  # DEBUG just an index or does it refer to the target frame
-                  tgt_idx = idx + j
-                  egomotion_data = results['egomotion'][0]
-                  # Insert target pose
-                  # DEBUG: check if the target pose is at the right index
-#                  egomotion_data = np.insert(egomotion_data, max_src_offset, np.zeros((1,6)), axis=0) 
-                  egomotion_data = np.insert(egomotion_data, 1, np.zeros((1,6)), axis=0) 
-#                  curr_times = times[tgt_idx - max_src_offset:tgt_idx + max_src_offset + 1]
-                  curr_times = times[tgt_idx - 1:tgt_idx + 1 + 1]
-                  #egomotion_file = output_dir + '%.6d.txt' % (tgt_idx - max_src_offset)
-                  egomotion_file = output_dir + '%.6d.txt' % (tgt_idx - 1)
-                  egomotion_path = os.path.join(FLAGS.output_dir, str(egomotion_file))
-                  print(egomotion_path)
-                  dump_pose_seq_TUM(egomotion_path, egomotion_data, curr_times)
-                  inf_egomotion_f.write("%s\n" % (egomotion_path))
+def load_sequence(dataset_dir, 
+                        frames, 
+                        tgt_idx, 
+                        seq_length)
+#    max_offset = int((seq_length - 1)/2)
+    max_offset = 1
+    for o in range(-max_offset, max_offset+1):
+        curr_idx = tgt_idx + o
+        curr_drive, curr_frame_id = frames[curr_idx].split(' ')
+        pose_file = os.path.join(
+            dataset_dir, 'sequences', '%s/image_2/%s.png' % (curr_drive, curr_frame_id))
+        curr_img = scipy.misc.imread(img_file)
+        if o == -max_offset:
+            image_seq = curr_img
+        else:
+            image_seq = np.hstack((image_seq, curr_img))
+    return image_seq
 
-                  # DEBUG : confirm if this is needed
-#                  if egomotion_file is not None:
-#                      egomotion_file.close()
-     
+
+def is_valid_sample(frames, tgt_idx, seq_length):
+    N = len(frames)
+
+    if tgt_idx >= N:
+      return False
+    tgt_drive, _ = frames[tgt_idx].split(' ')
+    #TODO: calculate max_offset in a clean way 
+#    max_offset = (seq_length - 1)//2
+    max_offset = 1
+    min_src_idx = tgt_idx - max_offset
+    max_src_idx = tgt_idx + max_offset
+    if min_src_idx < 0 or max_src_idx >= N:
+        return False
+    min_src_drive, _ = frames[min_src_idx].split(' ')
+    max_src_drive, _ = frames[max_src_idx].split(' ')
+    if tgt_drive == min_src_drive and tgt_drive == max_src_drive:
+        return True
+    return False
+
 
 def main(_):
-  _run_inference()
-
-
-if __name__ == '__main__':
-  app.run(main)
+  _gen_data()
