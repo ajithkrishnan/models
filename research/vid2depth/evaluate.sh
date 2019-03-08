1d2147e10aead7e2660e207a478d2e32166656b5
ajithkrishnan <ajithkrishnanbm@gmail.com>
2019-03-08 16:53:42 +0100

    Ground truth plots perfectly (same in normal and reversed matrix multiplication); ATE is now ~3 in both cases

--- research/vid2depth/evaluate.sh
+++ research/vid2depth/evaluate.sh
@@ -4,8 +4,8 @@ export PATH_VID2DEPTH=$(pwd)
 export PATH_KITTI_ODOM=$(pwd)/data
 
 python $PATH_VID2DEPTH/kitti_eval/eval_pose.py \
-    --pred_dir $PATH_VID2DEPTH/inference_evaluation_egomotion/ \
-    --gtruth_dir $PATH_KITTI_ODOM/dataset/ground_truth_processed/ 
+    --pred_dir $PATH_VID2DEPTH/inference_evaluation_egomotion_reverse/ \
+    --gtruth_dir $PATH_KITTI_ODOM/dataset/ground_truth_processed_reverse/ 
 
 #    --gtruth_dir $PATH_KITTI_ODOM/pose_data_processed/ground_truth/
 #python $PATH_VID2DEPTH/kitti_eval/evaluate.py \
--- research/vid2depth/plot.sh
+++ research/vid2depth/plot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 python plot_trajectories/evaluate_ate.py \
-    --first_file data/dataset/ground_truth_processed_plot/groundtruth.txt \
+    --first_file data/dataset/ground_truth_processed/groundtruth.txt \
     --second_file inference_evaluation_egomotion/inference.txt \
-    --plot PLOT_full \
+    --plot PLOT \
    --verbose 
 
 
